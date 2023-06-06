ca599675012d29194419515836e7d80438d7d868
danielmegale <danim1@live.it>
2023-06-06 18:53:01 +0200

    added function even odd

--- function.js
+++ function.js
@@ -8,11 +8,31 @@ function reverseWord(Word){
         result+=reverse.charAt(i);
     }
     if(result === reverse){
-        stampWord.innerHTML+=`<p>La parola è palindroma: ${result}</p>`
+        stampWord.innerHTML+=`<p>La parola è palindroma: ${result}</p>`;
     }else{
-        stampWord.innerHTML+=`<p>La parola al contrario è: ${result}</p>`
+        stampWord.innerHTML+=`<p>La parola al contrario è: ${result}</p>`;
     }
 }
 
 
 //Funzione pari o dispari
+function eveonOdd(shot){
+    const a=parseInt(shot)
+    console.log(shot)
+    if(a>5){
+        console.log('valore errato')
+    }else{
+        const pcThrow=Math.floor(Math.random() * 5 +1);
+        let sum= a + pcThrow;    
+        console.log(sum);
+        let evenOdd=option.value;
+        if(evenOdd == (sum % 2)){
+            stampthrow.innerHTML+=`<p>Hai vinto</p>`;
+            console.log(stampthrow);
+            return 0;
+        }else{
+            stampthrow.innerHTML+=`<p>Hai perso</p>`;
+            console.log();
+        }
+    }
+}
--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 // Prendo gli elementi dal DOM
-const userWord= document.getElementById('pali');
-const userThrow=document.getElementById('throw');
+const userWord =document.getElementById('pali');
+const userThrow =document.getElementById('user-throw');
 const wordBtn =document.getElementById('pali-btn');
 const throwBtn =document.getElementById('throw-btn');
-const stampWord=document.getElementById('word')
-const evenOdd=document.getElementById('even-odd')
-
+const stampWord =document.getElementById('word')
+const option =document.getElementById('even-odd')
+const stampthrow =document.getElementById('throw')
 
 wordBtn.addEventListener('click',function(){
-    reverseWord(userWord.value)
+    reverseWord(userWord.value);
 })
 
 
 
 
 throwBtn.addEventListener('click',function(){
-    const pcThrow=Math.floor(Math.random() * 5 +1)
-    let sum= userThrow + pcThrow
-    console.log(userThrow.value,pcThrow)    
-    
+    eveonOdd(userThrow.value);
 })
